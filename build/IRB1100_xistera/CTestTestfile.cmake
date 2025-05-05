5722609a79d8d96a499de4b54d8639461169bcdc
DavidSeyserGit <david.seyser@hotmail.com>
2025-05-05 12:54:10 +0200

    removed build folder

--- build/IRB1100_xistera/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/e/robotthrow_ws/src/IRB1100_xistera
-# Build directory: /mnt/e/robotthrow_ws/build/IRB1100_xistera
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
